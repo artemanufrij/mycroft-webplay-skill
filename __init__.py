d9726a72408c2576a49a79ffb2ff4d906fee0e66
Artem Anufrij <artem.anufrij@live.de>
2020-01-01 23:25:08 +0100

    umstellung auf audioservice

--- __init__.py
+++ __init__.py
@@ -1,13 +1,19 @@
 import os
 import subprocess
+import requests
+
+from adapt.intent import IntentBuilder
 
 from urllib.parse import quote
 
-from mycroft import intent_file_handler
+from mycroft import intent_file_handler, intent_handler
 from mycroft.audio import wait_while_speaking
 from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
 from mycroft.util import get_cache_directory
 from mycroft.util.parse import match_one
+
+mime = 'audio/mpeg'
+host = "http://localhost:31204/"
 
 
 class Webplay(CommonPlaySkill):
@@ -24,30 +30,44 @@ class Webplay(CommonPlaySkill):
         url = data['track']
         self.audioservice.play(url)
 
-    @intent_file_handler('Random.intent')
-    def handle_random_play(self, message):
-        self.now_playing = True
-        self.speak_dialog("random")
-        url = "http://localhost:31204/api/album/teststring"
-        mime = 'audio/mpeg'
-
-        if os.path.exists(self.STREAM):
-            os.remove(self.STREAM)
-        os.mkfifo(self.STREAM)
-
-        self.log.debug('Running curl {}'.format(url))
-        args = ['curl', '--data', '--data "param1=value1&param2=value2"',
-                '-L', quote(url, safe=":/"), '-o', self.STREAM]
-        self.curl = subprocess.Popen(args)
-
-        wait_while_speaking()
-        self.CPS_play(('file://' + self.STREAM, mime))
-
     @intent_file_handler('Stop.intent')
     def handle_stop_playing(self, message):
-        if self.now_playing == True:
-            self.stop()
-        self.now_playing = False
+        self.stop()
+
+    @intent_file_handler('Random.intent')
+    def handle_random_play(self, message):
+        self.speak_dialog("random")
+
+    @intent_handler(IntentBuilder("").require("Album").
+                    require("AlbumTitle"))
+    def handler_album_play(self, message):
+        self.speak_dialog("album")
+        url = host + "api/album/" + \
+            message.data.get("AlbumTitle")
+
+        r = requests.get(url)
+        try:
+            response = r.json()
+            track_id = response["tracks"][0]["_id"]
+            self.play_track_id(track_id)
+        except:
+            self.speak_dialog("nothing.found")
+
+    def play_track_id(self, id):
+        url = host + "api/track/" + id + "/stream/128"
+        print(url)
+
+        #if os.path.exists(self.STREAM):
+        #    os.remove(self.STREAM)
+        #os.mkfifo(self.STREAM)
+
+        #args = ['curl',
+        #        '-L', quote(url, safe=":/"), '-o', self.STREAM]
+        #self.curl = subprocess.Popen(args)
+
+        wait_while_speaking()
+        # self.CPS_play(('file://' + self.STREAM, mime))
+        self.audioservice.play(url)
 
 
 def create_skill():
